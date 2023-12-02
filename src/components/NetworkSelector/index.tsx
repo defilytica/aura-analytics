9379d2d11cf1e9448fe7f680179e30cbcdb3609c
Xeonus <vanderbourg@gmail.com>
2023-12-02 09:26:07 -0500

    FEATURE: add support for zkEVM

--- src/components/NetworkSelector/index.tsx
+++ src/components/NetworkSelector/index.tsx
@@ -6,7 +6,7 @@ import {
     GnosisNetworkInfo,
     NetworkInfo,
     OptimismNetworkInfo,
-    PolygonNetworkInfo,
+    PolygonNetworkInfo, PolygonZkEVMNetworkInfo,
 } from "../../constants/networks"
 import {useActiveNetworkVersion} from "../../state/application/hooks"
 import ArbitrumLogo from '../../assets/svg/arbitrum.svg'
@@ -15,6 +15,7 @@ import EtherLogo from '../../assets/svg/ethereum.svg'
 import OpLogo from '../../assets/svg/optimism.svg'
 import GnosisLogo from '../../assets/svg/gnosis.svg'
 import BaseLogo from '../../assets/svg/base.svg'
+import ZkEvmLogo from '../../assets/svg/zkevm.svg'
 import {useLocation, useNavigate} from "react-router-dom";
 import {useSwitchNetwork} from 'wagmi'
 
@@ -28,6 +29,8 @@ const updatePathForNetwork = (network: NetworkInfo, currentPath: string) => {
     if (currentPath === '/') {
         if (network === OptimismNetworkInfo) {
             newPath = 'optimism/pools';
+        } else if (network == PolygonZkEVMNetworkInfo) {
+            newPath = 'zkevm/pools';
         }
         newPath =`/${network.name.toLowerCase()}/pools`
         return newPath;
@@ -37,10 +40,11 @@ const updatePathForNetwork = (network: NetworkInfo, currentPath: string) => {
         newPath = `/${pathParts[pathParts.length - 1]}`;
     } else if (network === OptimismNetworkInfo) {
         newPath = `/optimism/${pathParts[pathParts.length - 1]}`;
+    } else if (network === PolygonZkEVMNetworkInfo) {
+        newPath = `/zkevm/${pathParts[pathParts.length - 1]}`;
     } else {
         newPath = `/${network.name.toLowerCase()}/${pathParts[pathParts.length - 1]}`;
     }
-
     return newPath;
 };
 
@@ -66,6 +70,10 @@ export default function NetworkSelector() {
             update(PolygonNetworkInfo)
             const newPath = updatePathForNetwork(PolygonNetworkInfo, location.pathname)
             navigate(newPath)
+        } else if (chainId === PolygonZkEVMNetworkInfo.chainId) {
+                update(PolygonZkEVMNetworkInfo)
+                const newPath = updatePathForNetwork(PolygonZkEVMNetworkInfo, location.pathname)
+                navigate(newPath)
         } else if (chainId === GnosisNetworkInfo.chainId) {
             update(GnosisNetworkInfo)
             const newPath = updatePathForNetwork(GnosisNetworkInfo, location.pathname)
@@ -146,6 +154,22 @@ export default function NetworkSelector() {
                         </Box>
                         <Box>
                             Polygon
+                        </Box>
+                    </Box>
+                </MenuItem>
+                <MenuItem value={PolygonZkEVMNetworkInfo.chainId} key="poly">
+                    <Box display="flex" alignItems="center">
+                        <Box mr={0.5}>
+                            <Avatar
+                                sx={{
+                                    height: 20,
+                                    width: 20
+                                }}
+                                src={ZkEvmLogo}
+                            />
+                        </Box>
+                        <Box>
+                            zkEVM
                         </Box>
                     </Box>
                 </MenuItem>
